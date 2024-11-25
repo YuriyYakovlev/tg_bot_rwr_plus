00a46fc6bc96c78e8c530d93a8e4bd4b9f09d105
Yuriy Yakovlev <yakovlev.yuriy@gmail.com>
2024-11-25 14:48:21 +0100

    refine queries

--- services/knowledgeSearchService.js
+++ services/knowledgeSearchService.js
@@ -1,16 +1,16 @@
 // knowledgeSearchService.js
 const { SessionsClient } = require("@google-cloud/dialogflow-cx");
-// const queryRefinementService = require("./queryRefinementService");
+const queryRefinementService = require("./queryRefinementService");
 
 const client = new SessionsClient();
 async function getAnswer(bot, chatId, userId, text) {
   bot.sendChatAction(chatId, "typing");
-  // const refinedQuery = await queryRefinementService.refineQuery(text);
-  // if(!refinedQuery) {
-  //   return null;
-  // }
+  const refinedQuery = await queryRefinementService.refineQuery(text);
+  if(!refinedQuery) {
+    return null;
+  }
 
-  // console.log(`refined query: ${refinedQuery}`);
+  console.log(`refined query: ${refinedQuery}`);
   let typingInterval = setInterval(() => {
     bot.sendChatAction(chatId, "typing");
   }, 4000);
